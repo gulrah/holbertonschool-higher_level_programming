b5686466dd26f66576bb4818dce0d6b78e9c7b92
gulrah <gulnarrahimli@mail.ru>
2024-01-26 06:30:26 -0800

    fizzbuzz_correct

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for x in range(100):
-    if x % 3 == 0 and x % 3 == 0:
+for x in range(101):
+    if x % 3 == 0 and x % 5 == 0:
         print(" FizzBuzz", end='')
     elif x % 3 == 0:
         print(" Fizz", end='')
