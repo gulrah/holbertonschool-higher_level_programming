9c0951bde9d0d1c7fc6b142e1fe733aeb68a6697
gulrah <gulnarrahimli@mail.ru>
2024-01-26 07:49:50 -0800

    jj

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for x in range (10):
     for y in range(x+1, 10):
-        print("{}{}".format(x, y), end=', ')
+        sep = ", " if x < 8 or y < 9 else "\n"
+        print("{}{}".format(x, y), end=sep)
