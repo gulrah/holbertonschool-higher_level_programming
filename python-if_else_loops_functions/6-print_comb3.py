fda2d84d100dfccbddbbf414d7404f3a05d854b7
gulrah <gulnarrahimli@mail.ru>
2024-01-26 07:40:49 -0800

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for x in range (10):
-    for y in range(10):
-        if x != y or y != x:
-            print("{}{}".format(x, y), end=', ')
+    for y in range(x+1, 10):
+        print("{}{}".format(x, y), end=', ')
