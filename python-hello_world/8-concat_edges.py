94475cde60f167da452283197d6186ffe63d2f45
gulrah <gulnarrahimli@mail.ru>
2024-01-28 11:27:23 -0800

    Deleted

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[65:81] + str[17:20] + str[0:6])
+str = str[39:67] + str[107:112] + str[0:6])
 print(str)
