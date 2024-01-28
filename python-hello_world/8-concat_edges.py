383b3642bb58bffb2439e0f3f2c48d1c59b9b33b
gulrah <gulnarrahimli@mail.ru>
2024-01-28 11:23:36 -0800

    New

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[65:81] + str[17:20] + str[0:6])
-
