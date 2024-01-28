83465baa9d127cc62988ae6044ff983f339dc0d3
gulrah <gulnarrahimli@mail.ru>
2024-01-28 07:14:45 -0800

    Printing list of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("0 arguments.")
+        print("{} arguments.".format(len(argv) - 1))
     elif len(argv) == 2:
-        print("1 argument:")
+        print("{} argument:".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+    for x in range(1, len(argv)):
+        print("{:d}: {}".format(x, argv[x]))
                 
