db1fd31a7215f085463e9e357bac4212a374dfeb
gulrah <gulnarrahimli@mail.ru>
2024-01-28 00:40:31 -0800

    Switching values

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     for x in my_list:
-        result = x % 2 == 0
-        return result
+        if x % 2 == 0:
+            return True
+        else:
+            return False
