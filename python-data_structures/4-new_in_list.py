bff637f8cd8bccaa1e8c89a246ebc59d07985b84
gulrah <gulnarrahimli@mail.ru>
2024-01-28 08:12:57 -0800

    Element in the new list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if 0 > idx > len(my_list):
-    mx    return my_list.copy()
+        return my_list.copy()
     new_list = my_list.copy()
     new_list[idx] = element
     return new_list
