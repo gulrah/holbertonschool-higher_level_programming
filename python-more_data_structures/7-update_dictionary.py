5f7603b1e5df959efae28cb40b403615fa42ca79
gulrah <gulnarrahimli@mail.ru>
2024-01-30 10:57:36 -0800

    Updating dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,4 @@ def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
     a_dictionary[key] = value
     else:
-        a_dictionary.update({key: value})
+        a_dictionary[key] = value
