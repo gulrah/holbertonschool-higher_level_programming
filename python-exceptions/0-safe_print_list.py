067bf000258b8dd4a14fa585d461a85c92ca71f2
gulrah <gulnarrahimli@mail.ru>
2024-02-04 11:15:26 -0800

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    idx = 0
+def safe_print_list(a=[], b=0):
+    c = 0
     try:
-        for i in my_list:
-            if idx < x:
-                print('{}'.format(my_list[idx]), end='')
-                idx += 1
+        for i in range(b):
+            print(a[i], end='')
+            c += 1
+    except IndexError:
+        pass
+    finally:
         print()
-     except TypeError:
-         pass
-     finally:
-        return idx
+    return c
