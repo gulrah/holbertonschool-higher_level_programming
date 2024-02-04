d1085e84d405d894bea58224ede9d184cf88ca4f
gulrah <gulnarrahimli@mail.ru>
2024-02-04 09:11:35 -0800

    documenatation edited

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ Module to add two integers
 """
 
 def add_integer(a, b=98):
-        """
+            """
     Adds two integers
 
     Arguments:
@@ -14,9 +14,8 @@ def add_integer(a, b=98):
     Returns:
     int: The addition of a and b
     """
-            if not isinstance(a, (int, float)):
-                    raise TypeError("a must be an integer")
-                    if not isinstance(b, (int, float)):
-                            raise TypeError("b must be an integer")
-                            return int(a) + int(b)
-                        
+    if not isinstance(a, (int, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
