a536b7f3430efa03fc8679ea0e632df5a4cc94ad
gulrah <gulnarrahimli@mail.ru>
2024-02-04 11:08:44 -0800

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    counter = 0
     for i in range(x):
         try:
-            if not isinstance(my_list[i], int):
-                raise ValueError("Value is not an integer")
             print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (IndexError, ValueError):
+            counter += 1
+        except (TypeError, ValueError):
             pass
-    print()
-    return count
+        print()
+        return counter
