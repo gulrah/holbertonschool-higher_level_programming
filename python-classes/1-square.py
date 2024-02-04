c2991125526c59d903f63ca42e86d4ce13c9b096
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-04 12:00:00 -0800

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-This module defines a class Square with a private instance attribute"""
+"""This module defines a class Square with a private instance attribute"""
+
+
 class Square:
-        """
-    This class represents a square with a specified size.
-    """
+    """This class represents a square with a specified size."""
     def __init__(self, size):
-                        """
-        Initializes a new instance of Square with a given size.
-        """
+        """Initializes a new instance of Square with a given size."""
         self.__size = size
