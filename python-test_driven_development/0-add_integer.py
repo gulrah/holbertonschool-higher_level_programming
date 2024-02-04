f229afead232c3d2b29fd41b9077779b0ad15f5f
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-04 11:50:22 -0800

    Update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,15 +4,14 @@
     Function that adds 2 integers
 """
 
-
-def add_integer(a, b=98):
+def add_integer(num1, num2=98):
     """
-    Returns an integer: the addition of a and b
+    Returns an integer: the addition of num1 and num2
     """
-    if not (isinstance(a, int) or isinstance(a, float)):
-        raise TypeError("a must be an integer")
-    if not (isinstance(b, int) or isinstance(b, float)):
-        raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a+b
+    if not (isinstance(num1, int) or isinstance(num1, float)):
+        raise TypeError("num1 must be an integer")
+    if not (isinstance(num2, int) or isinstance(num2, float)):
+        raise TypeError("num2 must be an integer")
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 + num2
