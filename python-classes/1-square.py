0d919467a6ed916b12a1e768d478279d3b612e8c
gulrah <gulnarrahimli@mail.ru>
2024-02-04 11:22:34 -0800

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a Square class with a size attribute."""
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+    """itializes a new Square instance."""
+        self.__size = size
