78d1d288afce2e3003c191fd704ca9f02904966e
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-05 16:58:37 +0400

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,4 +12,30 @@ class Square:
         Args:
             size (int): The size of the square (default is 0).
         """
-        self.__size = size
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If the provided value is not an integer.
+            ValueError: If the provided value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
