5982d5e98980ad1ee011cb2688045390e7051e4b
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-05 17:03:02 +0400

    Created 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""3-square.py - Defines a Square class with area calculation."""
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """Initializes a Square instance with an optional size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
