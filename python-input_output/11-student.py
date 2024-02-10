b3b28b852e9f68136e2b2852054f12d696c35130
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 17:23:50 +0400

    Module documentation is corrected

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,21 +1,48 @@
-#!/usr/bin/python3
 class Student:
-    """Defines a student."""
+    """
+    Represents a student with first name, last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance."""
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of a Student instance."""
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list, optional): A list of attributes to retrieve.
+                If None, retrieves all attributes.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
         if attrs is None:
             return self.__dict__
         else:
             return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student instance."""
+        """
+        Replaces all attributes of the Student instance with values from a dictionary.
+
+        Args:
+            json (dict): A dictionary containing attribute-value pairs.
+        """
         for key, value in json.items():
             setattr(self, key, value)
