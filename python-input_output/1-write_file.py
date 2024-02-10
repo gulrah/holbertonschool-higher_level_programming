cbe0e76fbda89799c72745f3b3ce25f8602888c8
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 16:42:55 +0400

    Shorter lines

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Writes a string to a text file (UTF8) and returns the number of characters written.
+Writes a string to a text file (UTF8) and returns the number of characters.
 """
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns the number of characters written."""
+    """Writes a string to a text file and returns the number of characters."""
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
