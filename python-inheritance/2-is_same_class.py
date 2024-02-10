bb5be6fffe11522930e913b3d06551468fb420f7
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 13:14:52 +0400

    is_same_class module

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_same_class module"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance of the specified class"""
+    return type(obj) == a_class
