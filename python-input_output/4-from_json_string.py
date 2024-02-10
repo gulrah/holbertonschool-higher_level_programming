f2d6786862af9f08afddafad6bf53a22534d9591
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 16:46:34 +0400

    Returns an object represented by a JSON string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,10 +8,3 @@ def from_json_string(my_str):
     """Returns an object represented by a JSON string."""
     import json
     return json.loads(my_str)
-
-
-if __name__ == "__main__":
-    s_my_list = "[1, 2, 3]"
-    my_list = from_json_string(s_my_list)
-    print(my_list)
-    print(type(my_list))
