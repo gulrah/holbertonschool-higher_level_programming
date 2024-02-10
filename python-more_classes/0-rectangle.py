8ea0f5971b251d978db2ebe96c8177c0baf02bc0
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 18:01:14 +0400

    Define a rectangle class

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Defines a Rectangle class.
+"""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self):
+        """Initializes an empty rectangle."""
+        pass
