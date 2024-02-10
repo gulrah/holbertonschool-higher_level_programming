b9bd3e1e55b4e6dd4eef0974568738bc4d2c0a70
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 13:29:02 +0400

    Shorter lines PEP8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,6 +3,6 @@
 
 
 def inherits_from(obj, a_class):
-    """Returns True if the object is an instance of a class that inherited (directly
-    or indirectly) from the specified class"""
+    """Returns True if the object is an instance of a class that 
+    inherited (directly or indirectly) from the specified class"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
