986636af4c27d42703b338ad1ca5932fefb8c07f
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 17:14:36 +0400

    Deleting whitespaces

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -19,4 +19,4 @@ class Student:
             return self.__dict__
         else:
             return {attr: getattr(self, attr) \
-                    for attr in attrs if hasattr(self, attr)}
+            for attr in attrs if hasattr(self, attr)}
