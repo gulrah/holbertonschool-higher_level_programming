ca6213ec53f087d9f4fed0dca589976d7f07a4e7
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 13:17:38 +0400

    is_kind_of_class module

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""is_kind_of_class module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object is an instance
+    of a class that inherited from, the specified class"""
+    return isinstance(obj, a_class)
