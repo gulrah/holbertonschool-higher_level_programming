aaad177a8741777ce66bc79daacd4825d5d4e2ca
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 17:07:41 +0400

    Reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Reads a text file (UTF8) and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
-  
+    """Reads a text file and prints its content"""
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
