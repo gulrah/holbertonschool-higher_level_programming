a86a532dcbbd6e6d597f19fa22961cb4c5b72dba
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 17:25:42 +0400

    Updates

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module that defines the Student class for representing students.
+"""
+
 class Student:
     """
     Represents a student with first name, last name, and age.
