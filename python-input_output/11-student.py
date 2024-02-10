0ca709c6dfdb63280a76504b9b69df20bc18cacf
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-10 17:34:53 +0400

    Updates

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -40,11 +40,14 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {
+                attr: getattr(self, attr) 
+                for attr in attrs if hasattr(self, attr)
+            }
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance with values from a dictionary.
+        Replaces all attributes of the Student instance with values.
 
         Args:
             json (dict): A dictionary containing attribute-value pairs.
