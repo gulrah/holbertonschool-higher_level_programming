e75edbb73b8bdd66258ec6cbbf59ac8f0d8f385e
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-11 22:48:54 +0400

    PEP8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Check if obj is an instance of a class inherited from the class.
-"""
+"""Check if obj is an instance of a class inherited from the specified class."""
 
 
 def inherits_from(obj, a_class):
-    """ Check for direct or indirect inheritance """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    """Check for direct or indirect inheritance."""
+    return isinstance(obj, a_class) and type(obj) is not a_class
