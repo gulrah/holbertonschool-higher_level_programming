8c00d3db95135321a66feb9b3a6fb2c954790099
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-11 22:51:00 +0400

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-"""BaseGeometry module"""
+"""BaseGeometry Module
+"""
 
 
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """Raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    """ Rectangle inherits from BaseGeomtry """
 
     def __init__(self, width, height):
-        """Initialize Rectangle"""
+        """ Constructor"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def __str__(self):
-        """Returns string representation of Rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-    def area(self):
-        """Returns the area of the rectangle"""
-        return self.__width * self.__height
