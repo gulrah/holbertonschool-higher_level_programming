e05153d12be7bce00926bcd5faca53afb5482aeb
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-11 22:09:46 +0400

    Update

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -75,7 +75,7 @@ class Rectangle:
 
     def __repr__(self):
         """Return the representation of the rectangle."""
-        return "<{}({}, {})>".format(type(self).__name__, self.__width, self.__height)
+        return "<{}({},{})>".format(self.__class__.__module__ + '.' + self.__class__.__name__, self.__width, self.__height)
 
 
 if __name__ == "__main__":
