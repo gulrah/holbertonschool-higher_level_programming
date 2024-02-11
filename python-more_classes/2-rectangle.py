c134d8f7d4deefb2a41e16274b2275c5d1d9a402
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-11 22:03:53 +0400

    Updates

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -62,8 +62,10 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
-        return 2 * (self.__width + self.__height)
+    """Return the perimeter of the rectangle."""
+    if self.__width == 0 or self.__height == 0:
+        return 0
+    return 2 * (self.__width + self.__height)
 
 
 if __name__ == "__main__":
