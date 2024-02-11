2d3a4bd5cb47c8c4b168ec6a2a9f9ab891480b0f
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-11 22:08:32 +0400

    Update

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -75,19 +75,9 @@ class Rectangle:
 
     def __repr__(self):
         """Return the representation of the rectangle."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "<{}({}, {})>".format(type(self).__name__, self.__width, self.__height)
 
 
 if __name__ == "__main__":
     my_rectangle = Rectangle(2, 4)
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-    print(str(my_rectangle))
-    print(repr(my_rectangle))
-
-    print("--")
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print(my_rectangle)
     print(repr(my_rectangle))
