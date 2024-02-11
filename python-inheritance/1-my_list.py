e76b6339450674e465794dfbcec6f46b425c7fdd
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-11 22:29:29 +0400

    Update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""Module for MyList class."""
+"""
+Module for MyList class.
+"""
 
 
 class MyList(list):
-    """A class that inherits from list with additional methods."""
+    """
+    Subclass of list.
+    """
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
+        """
+        Prints the list sorted in ascending order.
+        """
         print(sorted(self))
-
-
-if __name__ == "__main__":
-    MyList = __import__('1-my_list').MyList
-
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
