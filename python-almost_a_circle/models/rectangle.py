f1af8afefe76e0b6f5beded7d7433ebd2bdc4893
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-15 22:35:52 +0400

    1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,9 @@
 from models.base import Base
 
 class Rectangle(Base):
+    """Rectangle class, inherits from Base."""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle instance."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -12,48 +14,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getter for width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Width must be an integer")
-        if value <= 0:
-            raise ValueError("Width must be greater than 0")
+        """Setter for width."""
         self.__width = value
 
     @property
     def height(self):
+        """Getter for height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Height must be an integer")
-        if value <= 0:
-            raise ValueError("Height must be greater than 0")
+        """Setter for height."""
         self.__height = value
 
     @property
     def x(self):
+        """Getter for x."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x coordinate must be an integer")
-        if value < 0:
-            raise ValueError("x coordinate cannot be negative")
+        """Setter for x."""
         self.__x = value
 
     @property
     def y(self):
+        """Getter for y."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y coordinate must be an integer")
-        if value < 0:
-            raise ValueError("y coordinate cannot be negative")
+        """Setter for y."""
         self.__y = value
