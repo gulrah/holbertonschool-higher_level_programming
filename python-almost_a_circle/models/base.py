3502141ed7337b09aefdb57288cb88065360e446
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-16 13:18:12 +0400

    Base's update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for Base class."""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
