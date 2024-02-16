828c23dbecb7d8561e9bc8b32a5999ddb8810022
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-16 13:19:43 +0400

    Base's third update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,3 +30,10 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, "w") as file:
             file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of dictionaries represented by json_string."""
+        if not json_string:
+            return []
+        return json.loads(json_string)
