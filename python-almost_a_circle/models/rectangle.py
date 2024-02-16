e330f892c5d94ca1afca5ded439e1ec0af3e8ab5
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-16 13:16:26 +0400

    Rectangle's eighth update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,11 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        """Override __str__ method."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
     @property
     def width(self):
         """Getter for width."""
@@ -21,10 +26,6 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -35,10 +36,6 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -49,10 +46,6 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -63,34 +56,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
         self.__y = value
 
-    def area(self):
-        """Calculate and return the area of the rectangle."""
-        return self.width * self.height
-
-    def display(self):
-        """Print the Rectangle instance with '#' character and x, y offsets."""
-        for i in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def __str__(self):
-        """Override __str__ method."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height)
-
-    def update(self, *args, **kwargs):
-        """Update the attributes of the Rectangle."""
-        if args:
-            attrs = ['id', 'width', 'height', 'x', 'y']
-            for i, arg in enumerate(args):
-                setattr(self, attrs[i], arg)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
