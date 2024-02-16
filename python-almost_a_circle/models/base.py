312d8758aa1c8b53c33755559eee132e5ee35e69
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-16 13:23:43 +0400

    Base's fourth update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -39,3 +39,15 @@ class Base:
         if not json_string:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            return None
+        dummy_instance.update(**dictionary)
+        return dummy_instance
