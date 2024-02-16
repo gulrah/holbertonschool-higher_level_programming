a75a075730c7fe23cab05be17fd2c5fa53a87d92
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-16 13:15:03 +0400

    Square's second update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,40 +1,64 @@
 #!/usr/bin/python3
-"""Module for Square class."""
-from models.rectangle import Rectangle
+"""Module for Rectangle class."""
+from models.base import Base
 
 
-class Square(Rectangle):
-    """Square class, inherits from Rectangle."""
-    def __init__(self, size, x=0, y=0, id=None):
-        """Initialize Square instance."""
-        super().__init__(size, size, x, y, id)
+class Rectangle(Base):
+    """Rectangle class, inherits from Base."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle instance."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     def __str__(self):
         """Override __str__ method."""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
 
     @property
-    def size(self):
-        """Getter for size."""
-        return self.width
+    def width(self):
+        """Getter for width."""
+        return self.__width
 
-    @size.setter
-    def size(self, value):
-        """Setter for size."""
-        self.width = value
-        self.height = value
+    @width.setter
+    def width(self, value):
+        """Setter for width."""
+        self.__width = value
 
-    def update(self, *args, **kwargs):
-        """Update the attributes of the Square."""
-        if args:
-            attrs = ['id', 'size', 'x', 'y']
-            for i, arg in enumerate(args):
-                setattr(self, attrs[i], arg)
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    @property
+    def height(self):
+        """Getter for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y."""
+        self.__y = value
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Square."""
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        """Return the dictionary representation of a Rectangle."""
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
