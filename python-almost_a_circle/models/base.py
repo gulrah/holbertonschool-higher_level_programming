729257f2fe0bb4203db585e72dbecad6b40e2502
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-16 13:34:49 +0400

    3

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Module for Base class."""
-import json
-
 
 class Base:
     """Base class for managing id attribute."""
@@ -15,34 +12,33 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of list_dictionaries."""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file."""
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as file:
-            if list_objs is None:
-                file.write("[]")
-            else:
-                list_dicts = [obj.to_dictionary() for obj in list_objs]
-                file.write(cls.to_json_string(list_dicts))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Returns the list of the JSON string representation."""
-        if json_string is None or json_string == "":
-            return []
-        return json.loads(json_string)
-
     @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set."""
         instance = cls.__new__(cls)
         instance.__dict__.update(dictionary)
         return instance
+
+class Rectangle(Base):
+    """Rectangle class, inherits from Base."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle instance."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle."""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+
+if __name__ == "__main__":
+    r = Rectangle.create(**{'width': 2, 'height': 3})
+    print(r.to_dictionary())
