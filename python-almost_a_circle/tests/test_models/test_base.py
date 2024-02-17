2a16115f2267828ab2a1795ecbf8efae526690a7
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-17 17:13:11 +0400

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,16 +4,27 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_create_instance(self):
-        b = Base()
-        self.assertEqual(b.id, 1)
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_create_instance_increment_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
     def test_create_instance_with_id(self):
-        b = Base(10)
-        self.assertEqual(b.id, 10)
+        b = Base(89)
+        self.assertEqual(b.id, 89)
 
-    def test_create_instance_with_negative_id(self):
-        with self.assertRaises(ValueError):
-            b = Base(-1)
+    def test_to_json_string(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string('[{"id": 89 }]'), [{'id': 89}])
 
 
 if __name__ == '__main__':
