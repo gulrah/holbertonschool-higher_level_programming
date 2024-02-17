bfc98f550bfcea9df901d873277b1c642f5caf6a
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-17 18:23:07 +0400

    Update test_rectangle.py

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,37 +1,54 @@
 import unittest
-import io
-from unittest import mock
 from models.rectangle import Rectangle
+from models.base import Base
+from random import randrange
+from contextlib import redirect_stdout
+import io
 
 
 class TestRectangle(unittest.TestCase):
-    def test_width_height_validation(self):
-        with self.assertRaises(ValueError):
-            Rectangle(0, 1)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 0)
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
-    def test_x_y_validation(self):
-        with self.assertRaises(ValueError):
-            Rectangle(1, 1, -1)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 1, 0, -1)
+    def tearDown(self):
+        pass
 
-    def test_area(self):
-        r = Rectangle(5, 5)
-        self.assertEqual(r.area(), 25)
+    def test_class_type(self):
+        self.assertEqual(str(Rectangle), "<class 'models.rectangle.Rectangle'>")
 
-    def test_display(self):
-        r = Rectangle(3, 2)
-        expected_output = "###\n###\n"
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-            r.display()
-            self.assertEqual(fake_stdout.getvalue(), expected_output)
+    def test_inheritance(self):
+        self.assertTrue(issubclass(Rectangle, Base))
 
-    def test_update(self):
-        r = Rectangle(1, 1)
-        r.update(2, 2, 2, 2, 2)
-        self.assertEqual(str(r), "[Rectangle] (2) 2/2 - 2/2")
+    def test_constructor_no_args(self):
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle()
+        s = "__init__() missing 2 required positional arguments: 'width' and 'height'"
+        self.assertEqual(str(e.exception), s)
+
+    def test_constructor_many_args(self):
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle(1, 2, 3, 4, 5, 6)
+        s = "__init__() takes from 3 to 6 positional arguments but 7 were given"
+        self.assertEqual(str(e.exception), s)
+
+    def test_constructor_one_args(self):
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle(1)
+        s = "__init__() missing 1 required positional argument: 'height'"
+        self.assertEqual(str(e.exception), s)
+
+    def test_instantiation(self):
+        r = Rectangle(10, 20)
+        self.assertEqual(str(type(r)), "<class 'models.rectangle.Rectangle'>")
+        self.assertTrue(isinstance(r, Base))
+        d = {'_Rectangle__height': 20, '_Rectangle__width': 10,
+             '_Rectangle__x': 0, '_Rectangle__y': 0, 'id': 1}
+        self.assertDictEqual(r.__dict__, d)
+
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle("1", 2)
+        msg = "width must be an integer"
+        self.assertEqual(str(e.exception), msg)
 
     def test_to_dictionary(self):
         r = Rectangle(1, 2, 3, 4, 5)
@@ -39,7 +56,6 @@ class TestRectangle(unittest.TestCase):
             r.to_dictionary(),
             {'id': 5, 'width': 1, 'height': 2, 'x': 3, 'y': 4}
         )
-
 
 if __name__ == '__main__':
     unittest.main()
