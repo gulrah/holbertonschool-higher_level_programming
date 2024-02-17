742864123938cc31fccc4fdf0405b259fac4727a
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-17 16:41:37 +0400

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Defines a Square class that inherits from Rectangle."""
 
 from models.rectangle import Rectangle
-
 
 class Square(Rectangle):
     """Represents a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes a square.
+        """Initializes a Square instance.
 
         Args:
             size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position.
-            y (int, optional): The y-coordinate of the square's position.
+            x (int, optional): The x-coordinate of the square.
+            y (int, optional): The y-coordinate of the square.
             id (int, optional): The identifier of the square.
         """
         super().__init__(size, size, x, y, id)
@@ -29,14 +28,10 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Returns a string representation of the square."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-
     def update(self, *args, **kwargs):
         """Updates the attributes of the square."""
         if args:
-            attrs = ['id', 'size', 'x', 'y']
+            attrs = ["id", "size", "x", "y"]
             for i, arg in enumerate(args):
                 setattr(self, attrs[i], arg)
         else:
@@ -46,3 +41,4 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Returns the dictionary representation of the square."""
         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+
