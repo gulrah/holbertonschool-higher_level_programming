3cbca7242a1b26151f7768997705fe5b3b69ddc9
gulrah <146723639+gulrah@users.noreply.github.com>
2024-02-17 18:32:11 +0400

    Update test_rectangle.py

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,9 @@
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 import io
 from contextlib import redirect_stdout
+import os
 
 
 class TestRectangle(unittest.TestCase):
@@ -88,6 +90,57 @@ class TestRectangle(unittest.TestCase):
             with redirect_stdout(fake_stdout):
                 r.display()
             self.assertEqual(fake_stdout.getvalue(), expected_output)
+
+    def test_create(self):
+        r = Rectangle.create(**{'id': 89})
+        self.assertIsInstance(r, Rectangle)
+        self.assertEqual(r.id, 89)
+
+        r = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertIsInstance(r, Rectangle)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+
+        r = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertIsInstance(r, Rectangle)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+
+        r = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertIsInstance(r, Rectangle)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+
+        r = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertIsInstance(r, Rectangle)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
+
+    def test_save_to_file(self):
+        filename = "test_rectangle.json"
+        r = Rectangle(1, 2)
+        Rectangle.save_to_file([r])
+        self.assertTrue(os.path.isfile(filename))
+        os.remove(filename)
+
+    def test_load_from_file_nonexistent(self):
+        self.assertEqual(Rectangle.load_from_file(), [])
+
+    def test_load_from_file_existing(self):
+        filename = "test_rectangle.json"
+        r = Rectangle(1, 2)
+        Rectangle.save_to_file([r])
+        rectangles = Rectangle.load_from_file()
+        self.assertIsInstance(rectangles, list)
+        self.assertTrue(len(rectangles) == 1)
+        self.assertIsInstance(rectangles[0], Rectangle)
+        os.remove(filename)
 
 
 if __name__ == '__main__':
