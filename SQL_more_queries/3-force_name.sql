8283a78f0b8e80eb09fc8d130c43d3c7a3ab3b07
gulrah <gulnarrahimli@mail.ru>
2024-03-20 12:26:49 +0000

    Edits according to the server

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
 -- Creates the table force_name on the MySQL server.
 
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT,
         name VARCHAR(256) NOT NULL
 	);
 	
