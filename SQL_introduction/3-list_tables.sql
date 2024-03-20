6495640968a72dc1641f9fff413e8faf68218d13
gulrah <gulnarrahimli@mail.ru>
2024-03-20 09:12:12 +0000

    hot fix

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
 -- Script to list all tables in a database
-USE database_name;
 SHOW TABLES;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,5 @@
 -- Script to create a table called first_table
-USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
         name VARCHAR(256)
 	);
-	
