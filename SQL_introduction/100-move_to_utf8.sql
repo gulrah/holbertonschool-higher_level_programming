834f2247f4ba11ed1d5cd7f0f9b8e6fb4bcae524
gulrah <gulnarrahimli@mail.ru>
2024-03-20 09:54:22 +0000

    Adding value

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,2 @@
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Alter the table to use UTF8 encoding and collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- Script to display the full description of the table first_table
-DESCRIBE first_table;
+SHOW CREATE TABLE first_table;
