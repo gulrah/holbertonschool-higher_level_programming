9857d05a1847aac4802a1fb2e97b8445dc61f2ea
gulrah <gulnarrahimli@mail.ru>
2024-03-20 18:00:12 +0000

    Exceptions were added

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,33 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
+    # Check if the correct number of arguments is provided
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
+        
     # Connecting to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    except MySQLdb.Error as e:
+        print("Error: Unable to connect to MySQL database:", e)
+        exit(1)
+        
     # Creating cursor object
     cursor = db.cursor()
 
     # Executing SQL query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
+    try:
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    except MySQLdb.Error as e:
+        print("Error: Unable to execute SQL query:", e)
+        cursor.close()
+        db.close()
+        exit(1)
+        
     # Fetching all rows from the result set
     rows = cursor.fetchall()
-
+    
     # Printing results
     for row in rows:
         print(row)
