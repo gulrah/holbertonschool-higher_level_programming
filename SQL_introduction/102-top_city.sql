cf4b9e6847d7ddce2e36bc6d3a8a7083601825ba
gulrah <gulnarrahimli@mail.ru>
2024-03-20 09:30:13 +0000

    hot fix

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
--- Script to display the top 3 cities temperature during July and August ordered by temperature descending
-SELECT city, AVG(temperature) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+-- Script to display the top 3 cities' temperature during July and August ordered by temperature (descending)
+SELECT city, AVG(temperature) AS avg_temp FROM temperatures WHERE MONTH(date) IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script to list all records of the table second_table with names, ordered by descending score
-SELECT score, name FROM hbtn_0c_0.second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
