d73b1f96d786b3acf5e883a1abaa017a696299b6
gulrah <146723639+gulrah@users.noreply.github.com>
2024-03-20 11:04:08 -0700

    Exceptions

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
 """ Module to list all states """
 
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-
-    # Check if the correct number of arguments is provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(argv[0]))
-        exit(1)
-        
-    # Connecting to MySQL server
     try:
+        # Connecting to MySQL server
         db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    except MySQLdb.Error as e:
-        print("Error: Unable to connect to MySQL database:", e)
-        exit(1)
-        
-    # Creating cursor object
-    cursor = db.cursor()
 
-    # Executing SQL query
-    try:
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        # Creating cursor object
+        cursor = db.cursor()
+
+        # Executing SQL query
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetching all rows from the result set
+        rows = cursor.fetchall()
+
+        # Printing results
+        for row in rows:
+            print(row)
+        
     except MySQLdb.Error as e:
-        print("Error: Unable to execute SQL query:", e)
-        cursor.close()
-        db.close()
-        exit(1)
+        print("MySQL Error:", e)
         
-    # Fetching all rows from the result set
-    rows = cursor.fetchall()
-    
-    # Printing results
-    for row in rows:
-        print(row)
-        
-    # Closing cursor and database connection
-    cursor.close()
-    db.close()
+    finally:
+        # Closing cursor and database connection
+        if 'cursor' in locals():
+            cursor.close()
+        if 'db' in locals():
+            db.close()
