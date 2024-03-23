760966876d519f26bd2a92f607bfa1329fa773fb
gulrah <gulnarrahimli@mail.ru>
2024-03-23 12:10:44 +0000

    PEP8

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+Script that lists all states from the database hbtn_0e_0_usa.
 """
+
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-db=sys.argv[3], port=3306)
+if __name__ == "__main__":
+        # Get command line arguments
+        mysql_username = sys.argv[1]
+            mysql_password = sys.argv[2]
+                database_name = sys.argv[3]
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states;")
-states = cur.fetchall()
+                    # Connect to MySQL server
+                        db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                                                                      passwd=mysql_password, db=database_name)
 
-for state in states:
-        print(state)
+                            # Create a cursor object
+                                cursor = db.cursor()
+
+                                    # Execute the query
+                                        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+                                            # Fetch all the rows
+                                                rows = cursor.fetchall()
+
+                                                    # Display the results
+                                                        for row in rows:
+                                                                    print(row)
+
+                                                                        # Close cursor and database connection
+                                                                            cursor.close()
+                                                                                db.close()
+                                                                                
