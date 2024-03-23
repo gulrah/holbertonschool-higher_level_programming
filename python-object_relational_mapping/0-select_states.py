1e7b1e723763281f228f9bf7fa04964a9fa93b91
gulrah <gulnarrahimli@mail.ru>
2024-03-23 11:20:59 +0000

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""
-Script to list all states from hbtn_0e_0_usa database
-"""
+"""Lists all states from the database"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-            if len(sys.argv) != 4:
-                            print("Usage: {} username password database".format(sys.argv[0]))
-            else:
-                            db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-                                    cursor = db.cursor()
-                                            cursor.execute("SELECT * FROM states ORDER BY id ASC")
-                                                    for row in cursor.fetchall():
-                                                                        print(row)
-                                                                                cursor.close()
-                                                                                        db.close()
-                                                                                        
+                # Establish connection to MySQL database
+                db = MySQLdb.connect(
+                                    host="localhost",
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3]
+                                )
+
+                    # Create cursor object
+                        cursor = db.cursor()
+
+                            # Execute SQL query
+                                cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+                                    # Fetch and print results
+                                        for row in cursor.fetchall():
+                                                            print(row)
+
+                                                                # Close cursor and database connection
+                                                                    cursor.close()
+                                                                        db.close()
+                                                                        
