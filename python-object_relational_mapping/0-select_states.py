f181b661edeedac1b3d0d35c6fb3973a37e49848
gulrah <gulnarrahimli@mail.ru>
2024-03-23 12:12:58 +0000

    PEP8

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa.
-"""
-
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-        # Get command line arguments
-        mysql_username = sys.argv[1]
-            mysql_password = sys.argv[2]
-                database_name = sys.argv[3]
+            username = sys.argv[1]
+                password = sys.argv[2]
+                    database = sys.argv[3]
 
-                    # Connect to MySQL server
-                        db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                                                                      passwd=mysql_password, db=database_name)
-
-                            # Create a cursor object
-                                cursor = db.cursor()
-
-                                    # Execute the query
-                                        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-                                            # Fetch all the rows
-                                                rows = cursor.fetchall()
-
-                                                    # Display the results
-                                                        for row in rows:
-                                                                    print(row)
-
-                                                                        # Close cursor and database connection
-                                                                            cursor.close()
-                                                                                db.close()
-                                                                                
+                        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                                                      passwd=password, db=database, charset="utf8")
+                            cursor = db.cursor()
+                                cursor.execute("SELECT * FROM states ORDER BY id ASC")
+                                    rows = cursor.fetchall()
+                                        for row in rows:
+                                                        print(row)
+                                                            cursor.close()
+                                                                db.close()
+                                                                
