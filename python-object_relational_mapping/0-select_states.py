a091d9e194202f1d9fb92d8187979909967be01b
gulrah <gulnarrahimli@mail.ru>
2024-03-23 14:50:04 +0000

    Select States

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module to list all states """
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    try:
+    # Connecting to MySQL server using command-line arguments
+        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+        
+        # Creating cursor object
+        cursor = db.cursor()
+        
+        # Executing SQL query
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        
+        # Fetching all rows from the result set
+        rows = cursor.fetchall()
+        
+        # Printing results
+        for row in rows:
+            print(row)
+            
+    except MySQLdb.Error as e:
+        print("An error occurred:", e)
+    finally:
+    # Closing cursor and database connection
+        if 'cursor' in locals():
+            cursor.close()
+        if 'db' in locals():
+            db.close()
