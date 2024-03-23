5bc94c14f86b7e88d5f253a72ec7b264e074b4da
gulrah <gulnarrahimli@mail.ru>
2024-03-23 12:14:50 +0000

    PEP8

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-            username = sys.argv[1]
-                password = sys.argv[2]
-                    database = sys.argv[3]
 
-                        db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                                                                      passwd=password, db=database, charset="utf8")
-                            cursor = db.cursor()
-                                cursor.execute("SELECT * FROM states ORDER BY id ASC")
-                                    rows = cursor.fetchall()
-                                        for row in rows:
-                                                        print(row)
-                                                            cursor.close()
-                                                                db.close()
-                                                                
+def main():
+                """
+    Connects to the MySQL database and retrieves the list of states sorted by their IDs.
+    """
+                    if len(sys.argv) != 4:
+                                        print("Usage: {} username password database_name".format(sys.argv[0]))
+                                                sys.exit(1)
+
+                                                    username = sys.argv[1]
+                                                        password = sys.argv[2]
+                                                            database = sys.argv[3]
+
+                                                                try:
+                                                                                    db = MySQLdb.connect(
+                                                                                                            host="localhost",
+                                                                                                            port=3306,
+                                                                                                            user=username,
+                                                                                                            passwd=password,
+                                                                                                            db=database,
+                                                                                                            charset="utf8"
+                                                                                                        )
+                                                                                            cursor = db.cursor()
+                                                                                                    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+                                                                                                            rows = cursor.fetchall()
+                                                                                                                    for row in rows:
+                                                                                                                                            print(row)
+                                                                except MySQLdb.Error as e:
+                                                                                    print("MySQL Error: {}".format(e))
+                                                                                            sys.exit(1)
+                                                                finally:
+                                                                                    if db:
+                                                                                                            db.close()
+
+
+                                                                                                            if __name__ == "__main__":
+                                                                                                                            main()
+                                                                                                                            
