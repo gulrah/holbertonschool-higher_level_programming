604066b7cd684338d5ddb054538a883befa27ee4
gulrah <gulnarrahimli@mail.ru>
2024-03-23 14:38:31 +0000

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-                                                                    """  Open database connection """
-                                                                        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                                                                                                                      passwd=argv[2], db=argv[3])
+    import MySQLdb
+    from sys import argv
 
-                                                                            cursor = db.cursor()  # Prepare a cursor object using cursor() method
-                                                                                cursor.execute("SELECT * FROM states ORDER BY id ASC")  # Query the dbase
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    
+    cursor = db.cursor()
 
-                                                                                    all_rows = cursor.fetchall()  # Fetch all rows
-                                                                                        for row in all_rows:
-                                                                                                                                                                print(row)
-                                                                                                                                                                    cursor.close()  # Disconnect from server
-                                                                                                                                                                        db.close()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+        
+    if 'cursor' in locals():
+        cursor.close()
+    if 'db' in locals():
+        db.close()
