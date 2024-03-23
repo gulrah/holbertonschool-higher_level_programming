a76cf41c4a1c4d8e17f1d39e6ecaeabb191bff03
gulrah <gulnarrahimli@mail.ru>
2024-03-23 18:10:56 +0000

    Module to list all states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -2,11 +2,26 @@
 """ Module to filter states by user input """
 
 
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Connect to MySQL server
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+
+    # Create cursor
+    cursor = db.cursor()
+
+    # Execute SQL query
+    cursor.execute("SELECT * FROM `states`")
+
+    # Fetch all rows and filter by state name
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+            
+    # Close cursor and connection
+    cursor.close()
+    db.close()
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,40 +1,12 @@
-"""Safely queries the database for state names."""
+#!/usr/bin/python3
+""" Module to list all states """
+
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-        # Check for correct number of arguments
-        if len(sys.argv) != 5:
-                    print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Retrieve command line arguments
-    username, password, database_name, state_name = sys.argv[1:]
-
-    # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                             passwd=password, db=database_name, charset="utf8")
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    # Create cursor object
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-
-    # Execute safe query using parameters
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    try:
-        cursor.execute(query, (state_name,))
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        cursor.close()
-        db.close()
-        sys.exit(1)
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+    cursor.execute("SELECT * FROM `states` WHERE `name` = %s", (sys.argv[4],))
+    [print(state) for state in cursor.fetchall()]
