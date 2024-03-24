cd3923e796f648b32a1c479ef1c4b4508cf829f6
gulrah <gulnarrahimli@mail.ru>
2024-03-24 15:15:23 +0000

    Added Exceptions

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Module """
-
+"""Script that lists all cities from the database"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id ORDER BY c.id")
-    [print(city) for city in cursor.fetchall()]
-                    
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-""" Module """
-
-
+"""Script that lists all cities of a given state from the database"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT c.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id WHERE s.name = %s ORDER BY c.id", (sys.argv[4],))
-    print(", ".join([city[0] for city in cursor.fetchall()]))
+    if len(sys.argv) == 5:
+        try:
+            db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+            cursor = db.cursor()
+            cursor.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
+            result = cursor.fetchone()
+            if result[0]:
+                print(result[0])
+            else:
+                print()
+        except MySQLdb.Error as e:
+            print("MySQL Error:", e)
+        finally:
+            if db:
+                db.close()
+    else:
+        print("Usage: {} <username> <password> <database_name> <state_name>".format(sys.argv[0]))
