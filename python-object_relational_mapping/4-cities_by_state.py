8d7052eeb972aa4729a39e86dffecd0c1fbedac7
gulrah <gulnarrahimli@mail.ru>
2024-03-24 19:41:26 +0000

    fix

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id ORDER BY c.id")
+    [print(city) for city in cursor.fetchall()]
+                    
