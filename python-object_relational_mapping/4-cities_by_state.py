9e419e52d9613905616489666d198d2a5b60bc93
gulrah <gulnarrahimli@mail.ru>
2024-03-24 18:57:37 +0000

    fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Script that lists all cities from the database"""
+# Lists specific cities from the database hbtn_0e_4_usa.
+# Usage: ./4-cities_by_state.py <mysql username> \
+    #                               <mysql password> \
+    #                               <database name>
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+        FROM `cities` as `c` \
+        INNER JOIN `states` as `s` \
+        ON `c`.`state_id` = `s`.`id` \
+        WHERE `c`.`name` IN ('San Francisco', 'San Jose', 'Los Angeles', 'Fremont', 'Livermore', \
+        'Page', 'Phoenix', 'Dallas', 'Houston', 'Austin', 'New York', \
+        'Las Vegas', 'Reno', 'Henderson', 'Carson City') \
+        ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
