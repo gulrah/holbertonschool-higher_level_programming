0523f81899054449a8955060643be735f57f4c2e
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-03 04:05:03 -0700

    E501 Correct version

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,9 +6,21 @@ import MySQLdb
 if __name__ == "__main__":
     if len(sys.argv) == 5:
         try:
-            db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+            db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
+            )
             cursor = db.cursor()
-            cursor.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
+            cursor.execute("""
+                SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
+                FROM cities
+                JOIN states ON cities.state_id = states.id
+                WHERE states.name = %s
+                ORDER BY cities.id ASC
+            """, (sys.argv[4],))
             result = cursor.fetchone()
             if result[0]:
                 print(result[0])
